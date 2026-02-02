e4699722fa110f9b0525165e90cfc8728f4993c3
Mariana Fernandes <gonelf@gmail.com>
2026-02-02 18:21:06 +0000

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,8 +4,25 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
+const getDatabaseUrl = () => {
+  let url = process.env.DATABASE_URL
+  if (!url) return undefined
+
+  if (!url.includes('pgbouncer=true')) {
+    url += url.includes('?') ? '&' : '?'
+    url += 'pgbouncer=true'
+  }
+
+  return url
+}
+
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({
   log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  datasources: {
+    db: {
+      url: getDatabaseUrl(),
+    },
+  },
 })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
